5f973129b57b06d560011f0a17d059c888b8a86e
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-09-12 22:53:22 +0800

    4.5

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -19,7 +19,7 @@ public class Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/events")
+				registry.addMapping("/**")
 						.allowedOrigins("http: //localhost:5173");
 			}
 		};
