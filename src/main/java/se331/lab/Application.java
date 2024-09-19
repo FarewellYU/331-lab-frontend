e4a6abe0607f7b5904344ad10676d7fed5f961dc
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-09-19 23:17:46 +0800

    Lab08-2.4

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -12,8 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+		SpringApplication.run(Application.class, args);}
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
